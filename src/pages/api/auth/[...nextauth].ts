73983f449cbed2d20e555c88053dc61a6606b5e8
izelacerda <izelacerdajl@gmail.com>
2021-03-31 16:58:13 -0300

    add save faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,6 +14,38 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+            )
+          )
+        )
+        return true
+      } catch (error) {
+        return false
+      }
+     
+    },
+  }
 
   // A database is optional, but required to persist accounts in a database
   // database: process.env.DATABASE_URL,
