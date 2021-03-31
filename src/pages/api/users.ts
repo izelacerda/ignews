e9bd661adaff3b9d2a35c2d541a6f2d85f0f0669
izelacerda <izelacerdajl@gmail.com>
2021-03-31 11:24:49 -0300

    add api roots

---
+++ src/pages/api/users.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+export default (request: NextApiRequest , response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: 'Jose'},
+    { id: 2, name: 'Dani'},
+    { id: 3, name: 'Rafa'},
+  ]
+
+  return response.json(users);
+}
+
+// Serveless (todas as api)
