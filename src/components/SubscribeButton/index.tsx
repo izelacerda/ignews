55324650a9e66af55affc082fef2d4c814f7beaa
izelacerda <izelacerdajl@gmail.com>
2021-04-01 18:10:45 -0300

    add customer stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,38 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStrypeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
+
+// getServerSideProps (SSR)
+// getStatisProps (SSG) - Static Site Generation
+// Api Routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+      const stripe = await getStrypeJs()
+      await stripe.redirectToCheckout({ sessionId })
+    } catch(err) { 
+      console.log(err.message)
+    }
+  }
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={() => handleSubscribe()}
     >
       Subscribe now
     </button>
